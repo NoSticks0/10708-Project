205dc4cf81de1a340d134f623dfac378b2bd91c6
Nathan M <48233127+NoSticks0@users.noreply.github.com>
2023-04-26 00:45:31 -0400

    Start of data generation code

--- pysrc/constants.py
+++ pysrc/constants.py
@@ -3,3 +3,6 @@ from pathlib import Path
 cachepath = lambda x: (Path(__file__).parent.parent / "cache" / x).resolve()
 datapath = lambda x: (Path(__file__).parent.parent / "data" / x).resolve()
 chartpath = lambda x: (Path(__file__).parent.parent / "charts" / x).resolve()
+
+N_ITEMS = 91599
+N_USERS = 52643
